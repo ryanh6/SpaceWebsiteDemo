9d7e142423967f1ba049011b56c15769c178f7a0
Ryan Ho <ryanhohoho6@gmail.com>
2023-06-03 23:20:30 -0400

    made full array of all current images

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ $(document).ready(function()
 {
     //const array = [1,2,3,4,5,6,7,8,9];
     const imageArray = ["images/asteroid1.jpg", "images/asteroid2.jpg", "images/asteroid3.jpg", "images/asteroid4.jpg", "images/asteroidBanner.jpg",
-                        "images/blackHole1.webp", "images/blackHole2.jpg", "images/blackHole3.jfif", "images/blackHole4.jpg", "images/blackHoleBanner.jpg"];
+                        "images/blackHole1.webp", "images/blackHole2.jpg", "images/blackHole3.jfif", "images/blackHole4.jpg", "images/blackHoleBanner.jpg",
+                        "images/eclipse1.webp", "images/eclipse2.webp", "images/eclipse3.jpg", "images/eclipse4.jpg", "images/eclipseBanner.jpg",
+                        "images/EuropaMoon2.jpeg", "images/galaxy1.webp", "images/galaxy2.jfif", "images/galaxy3.webp", "images/galaxy4.jpg",
+                        "images/galaxyBanner.jpg", "images/GanymedeMoon1.jpg", "images/moon4.jpg", "images/moonBanner.jpg", "images/nebula1.jpg",
+                        "images/nebula2.jpg", "images/nebula3.jpg", "images/nebula4.webp", "images/nebulaBanner.jpg", "images/stars1.jpg",
+                        "images/stars2.jpg", "images/stars3.webp", "images/stars4.jpg", "images/starsBanner.jpg", "images/titanMoon3.jpeg"];
+
     const shuffledArray = imageArray.sort(function(){return 0.5 - Math.random()});
 
     $(".random").ready(function(){
-        for (let i = 0; i < 4; i++){
-            $(".random").html("<img class = \"galleryImage\" src = \"" + shuffledArray[i] + "\"/>");
+        for (let i = 0; i < 15; i++){
+            $(".random").append("<img class = \"galleryImage\" src = \"" + shuffledArray[i] + "\"/>");
         }
     });
 
