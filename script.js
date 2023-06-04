e9c81434e094e66769a7ddd41fc3c0f650dc250b
Ryan Ho <ryanhohoho6@gmail.com>
2023-06-03 23:10:49 -0400

    changed random numbers to random images

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(function()
 {
-    const array = [1,2,3,4,5,6,7,8,9];
-    const shuffledArray = array.sort(function(){return 0.5 - Math.random()});
+    //const array = [1,2,3,4,5,6,7,8,9];
+    const imageArray = ["images/asteroid1.jpg", "images/asteroid2.jpg", "images/asteroid3.jpg", "images/asteroid4.jpg", "images/asteroidBanner.jpg",
+                        "images/blackHole1.webp", "images/blackHole2.jpg", "images/blackHole3.jfif", "images/blackHole4.jpg", "images/blackHoleBanner.jpg"];
+    const shuffledArray = imageArray.sort(function(){return 0.5 - Math.random()});
 
     $(".random").ready(function(){
-        $("p").html(shuffledArray);
+        for (let i = 0; i < 4; i++){
+            $(".random").html("<img class = \"galleryImage\" src = \"" + shuffledArray[i] + "\"/>");
+        }
     });
 
     $(".hiddenContent").hide();
