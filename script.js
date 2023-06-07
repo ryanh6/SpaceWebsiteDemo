8d57598ba3ad5ba38ae24286485d387200ec5f9d
Ryan Ho <ryanhohoho6@gmail.com>
2023-06-06 23:37:34 -0400

    added more images, added to gallery

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ $(document).ready(function()
                         "images/EuropaMoon2.jpeg", "images/galaxy1.webp", "images/galaxy2.jfif", "images/galaxy3.webp", "images/galaxy4.jpg",
                         "images/galaxyBanner.jpg", "images/GanymedeMoon1.jpg", "images/moon4.jpg", "images/moonBanner.jpg", "images/nebula1.jpg",
                         "images/nebula2.jpg", "images/nebula3.jpg", "images/nebula4.webp", "images/nebulaBanner.jpg", "images/stars1.jpg",
-                        "images/stars2.jpg", "images/stars3.webp", "images/stars4.jpg", "images/starsBanner.jpg", "images/titanMoon3.jpeg"];
+                        "images/stars2.jpg", "images/stars3.webp", "images/stars4.jpg", "images/starsBanner.jpg", "images/titanMoon3.jpeg",
+                        "images/earth1.webp", "images/jupiter1.jpg", "images/jupiter2.jpg", "images/mars1.jpg", "images/mars2.jpg",
+                        "images/mars3.jpg", "images/mercury1.jpg", "images/mercury2.avif", "images/neptune1.jpg", "images/neptune2.webp",
+                        "images/saturn1.jpg", "images/saturn2.jpg", "images/saturn.png", "images/uranus1.jpg", "images/venus1.jpg", "images/venus2.jpg"];
 
     const shuffledArray = imageArray.sort(function(){return 0.5 - Math.random()});
 
