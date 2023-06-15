fd6ed16b6a72b153bf21ba44462cd813c40f8c34
Ryan Ho <ryanhohoho6@gmail.com>
2023-06-15 17:47:08 -0400

    changed a few images, js for changing title page

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 $(document).ready(function()
 {
-    //const array = [1,2,3,4,5,6,7,8,9];
-    const imageArray = ["images/asteroid1.jpg", "images/asteroid2.jpg", "images/asteroid3.jpg", "images/asteroid4.jpg", "images/asteroidBanner.jpg",
-                        "images/blackHole1.webp", "images/blackHole2.jpg", "images/blackHole3.jfif", "images/blackHole4.jpg", "images/blackHoleBanner.jpg",
-                        "images/eclipse1.webp", "images/eclipse2.webp", "images/eclipse3.jpg", "images/eclipse4.jpg", "images/eclipseBanner.jpg",
-                        "images/EuropaMoon2.jpeg", "images/galaxy1.webp", "images/galaxy2.jfif", "images/galaxy3.webp", "images/galaxy4.jpg",
-                        "images/galaxyBanner.jpg", "images/GanymedeMoon1.jpg", "images/moon4.jpg", "images/moonBanner.jpg", "images/nebula1.jpg",
-                        "images/nebula2.jpg", "images/nebula3.jpg", "images/nebula4.webp", "images/nebulaBanner.jpg", "images/stars1.jpg",
-                        "images/stars2.jpg", "images/stars3.webp", "images/stars4.jpg", "images/starsBanner.jpg", "images/titanMoon3.jpeg",
-                        "images/earth1.webp", "images/jupiter1.jpg", "images/jupiter2.jpg", "images/mars1.jpg", "images/mars2.jpg",
-                        "images/mars3.jpg", "images/mercury1.jpg", "images/mercury2.avif", "images/neptune1.jpg", "images/neptune2.webp",
-                        "images/saturn1.jpg", "images/saturn2.jpg", "images/saturn3.png", "images/uranus1.jpg", "images/venus1.jpg", "images/venus2.jpg"];
+    const titleImageArray = ["images/spaceBackground1.webp", "images/spaceBackground2.jpg", "images/spaceBackground3.jpg", "images/spaceBackground4.jpg",
+                             "images/spaceBackground5.jpg", "images/spaceBackground6.jpg", "images/spaceBackground7.jpg", "images/spaceBackground8.jpg",
+                             "images/spaceBackground9.jpg", "images/spaceBackground10.jpg"];
+    const galleryImageArray = ["images/asteroid1.jpg", "images/asteroid2.jpg", "images/asteroid3.jpg", "images/asteroid4.jpg", "images/asteroidBanner.jpg",
+                               "images/blackHole1.webp", "images/blackHole2.jpg", "images/blackHole3.jfif", "images/blackHole4.jpg", "images/blackHoleBanner.jpg",
+                               "images/eclipse1.webp", "images/eclipse2.webp", "images/eclipse3.jpg", "images/eclipse4.jpg", "images/eclipseBanner.jpg",
+                               "images/EuropaMoon2.jpeg", "images/galaxy1.webp", "images/galaxy2.jfif", "images/galaxy3.webp", "images/galaxy4.jpg",
+                               "images/galaxyBanner.jpg", "images/GanymedeMoon1.jpg", "images/moon4.jpg", "images/moonBanner.jpg", "images/nebula1.jpg",
+                               "images/nebula2.jpg", "images/nebula3.jpg", "images/nebula4.webp", "images/nebulaBanner.jpg", "images/stars1.jpg",
+                               "images/stars2.jpg", "images/stars3.webp", "images/stars4.jpg", "images/starsBanner.jpg", "images/titanMoon3.jpeg",
+                               "images/earth1.webp", "images/jupiter1.jpg", "images/jupiter2.jpg", "images/mars1.jpg", "images/mars2.jpg",
+                               "images/mars3.jpg", "images/mercury1.jpg", "images/mercury2.avif", "images/neptune1.jpg", "images/neptune2.webp",
+                               "images/saturn1.jpg", "images/saturn2.jpg", "images/saturn3.png", "images/uranus1.jpg", "images/venus1.jpg", "images/venus2.jpg"];
 
-    const shuffledArray = imageArray.sort(function(){return 0.5 - Math.random()});
+    const shuffledTitleArray = titleImageArray.sort(function(){return 0.5 - Math.random()});
+    const shuffledGalleryArray = galleryImageArray.sort(function(){return 0.5 - Math.random()});
+
+    $("#titleBanner").ready(function(){
+        var titlePageImage = shuffledTitleArray[0];
+
+        $("#titleBanner").css({"background-image": "url(" + titlePageImage + ")"});
+    });
 
     $(".random").ready(function(){
         for (let i = 0; i < 20; i++){
-            $(".random").append("<img class = \"galleryImage\" src = \"" + shuffledArray[i] + "\"/>");
+            $(".random").append("<img class = \"galleryImage\" src = \"" + shuffledGalleryArray[i] + "\"/>");
         }
     });
 
